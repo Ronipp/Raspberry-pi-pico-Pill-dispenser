841ca10b3a5cdfbfb55621195d3113e6c69f7b44
Ronald5607 <roni.paloniemi@hotmail.com>
2023-12-18 12:34:19 +0200

    this builds

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 
 #include "hardware/pio.h"
 
-#define UART_TX_PIN
-#define UART_RX_PIN
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+#define STEPPER_OPTO_FORK_PIN 28
 
 typedef enum state {
     a,
@@ -22,12 +23,12 @@ typedef enum state {
 
 int main() {
     stdio_init_all();
-    eeprom_init_i2c(i2c0, 9600, 5);
+    eeprom_init_i2c(i2c0, 1000000, 5);
     lora_init(uart1, UART_TX_PIN, UART_RX_PIN);
-    stepper_ctx step_ctx = stepper_get_ctx();
-    stepper_init(&step_ctx, pio0, );
 
-    reboot_sequence();
+    uint stepperpins[4] = {BLUE, PINK, YELLOW, ORANGE};
+    stepper_ctx step_ctx = stepper_get_ctx();
+    stepper_init(&step_ctx, pio0, stepperpins, STEPPER_OPTO_FORK_PIN, 10, STEPPER_CLOCKWISE);
 
     state sm = a;
 
