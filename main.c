c7b87a49d4d86614222ca2911292bd54d13d1ec1
Ronald5607 <roni.paloniemi@hotmail.com>
2023-12-18 11:59:49 +0200

    gf

--- main.c
+++ main.c
@@ -10,11 +10,18 @@
 
 #include "hardware/pio.h"
 
+#define UART_TX_PIN
+#define UART_RX_PIN
 
 
 int main() {
     stdio_init_all();
-    
+    eeprom_init_i2c(i2c0, 9600, 5);
+    lora_init(uart1, UART_TX_PIN, UART_RX_PIN);
+    stepper_ctx step_ctx = stepper_get_ctx();
+    stepper_init(step_ctx, pio0, );
+
+    reboot_sequence();
 
     while (1) {
 
