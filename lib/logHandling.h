5f17ea8d2d29468cb565546a7a27797593386d62
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-23 04:33:45 +0200

    Update logHandling.h

--- lib/logHandling.h
+++ lib/logHandling.h
@@ -12,10 +12,11 @@ typedef struct DeviceStatus
     int unusedLogIndex; // index of log the program will use.
 } DeviceStatus;
 
+
 uint16_t crc16(const uint8_t *data, size_t length);
 void appendCrcToBase8Array(uint8_t *base8Array, int *arrayLen);
-int getChecksum(uint8_t *base8Array, int *arrayLen, bool flagArrayLenAsTerminatingZero);
-bool verifyDataIntegrity(uint8_t *base8Array, int *arrayLen, bool flagArrayLenAsTerminatingZero);
+int getChecksum(uint8_t *base8Array, int *arrayLen);
+bool verifyDataIntegrity(uint8_t *base8Array, int *arrayLen);
 void reboot_sequence(struct DeviceStatus *ptrToStruct, const uint64_t bootTimestamp);
 bool enterLogToEeprom(uint8_t *base8Array, int *arrayLen, int logAddr);
 void zeroAllLogs();
@@ -27,5 +28,7 @@ int findFirstAvailableLog();
 uint32_t getTimestampSinceBoot(const uint64_t bootTimestamp);
 void pushLogToEeprom(struct DeviceStatus *pillDispenserStatusStruct, int messageCode, uint64_t bootTimestamp);
 void updateUnusedLogIndex(struct DeviceStatus *pillDispenserStatusStruct);
+
+Ensure you have the necessary includes (such as stdint.h, stddef.h, and stdbool.h) to use the necessary types and boolean values (true and false). 
 
 #endif
