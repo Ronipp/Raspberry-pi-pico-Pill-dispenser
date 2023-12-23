7065549b6e170f3545ea42b027c05f081761c2a8
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-23 02:25:54 +0200

    fixed typo

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main()
     uint8_t data[EEPROM_ARR_LENGTH];
     uint64_t time = time_us_64();
     deviceStatus devStatus;
-    reboot_sequence(&devStatus, watchdog_caused_reboot());
+    reboot_sequence(&devStatus, watchdog_caused_reboot(), time);
 
     while (true){
         watchdog_update();
