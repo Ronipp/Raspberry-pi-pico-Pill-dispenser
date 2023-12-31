637344de584a76b2207e1cbfd535ef53d600e4f7
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-31 04:53:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -96,8 +96,8 @@ int main()
     reboot_sequence(&devStatus, bootTime);
 
     state_machine sm;
-    const int dispenseCodes = {DISPENSE1, DISPENSE2, DISPENSE3, DISPENSE4, DISPENSE5, DISPENSE6, DISPENSE7};
-    if (devStatus.rebootStatusCode !== devStatus.pillDispenseState && isValueInArray(devStatus.rebootStatusCode, dispenseCodes, 7) == true) {
+    const int dispenseCodes[7] = {DISPENSE1, DISPENSE2, DISPENSE3, DISPENSE4, DISPENSE5, DISPENSE6, DISPENSE7};
+    if (devStatus.rebootStatusCode != devStatus.pillDispenseState && isValueInArray(devStatus.rebootStatusCode, dispenseCodes, 7) == true) {
         sm = statemachine_get(devStatus.pillDispenseState, devStatus.rebootStatusCode);
     }
     else {
