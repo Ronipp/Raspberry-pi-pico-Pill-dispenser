be5a2d09b568ea89f3aa665d6f62f3c671d08233
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2023-12-31 08:17:05 +0200

    Update logHandling.c

--- src/logHandling.c
+++ src/logHandling.c
@@ -7,11 +7,11 @@
 #include "string.h"
 #include "../lib/logHandling.h"
 
-#define LOG_LEN 6               // Does not include CRC
+#define LOG_LEN 6                     // Does not include CRC
 #define LOG_ARR_LEN LOG_LEN + CRC_LEN // Includes CRC
 #define CRC_LEN 2
 
-#define DISPENSER_STATE_LEN 4                           // Does not include CRC
+#define DISPENSER_STATE_LEN 4                                 // Does not include CRC
 #define DISPENSER_STATE_ARR_LEN DISPENSER_STATE_LEN + CRC_LEN // Includes CRC
 #define PILL_DISPENSE_STATE 0
 #define REBOOT_STATUS_CODE 1
@@ -24,11 +24,9 @@
 #define LOG_SIZE 64
 #define MAX_LOGS 32
 
-
-
 const char *logMessages[] = {
     "Boot Finished",
-    "Watchdog caused reboot",    
+    "Watchdog caused reboot",
     "Dispensing pill 1",
     "Dispensing pill 2",
     "Dispensing pill 3",
@@ -86,7 +84,6 @@ void appendCrcToBase8Array(uint8_t *base8Array, int *arrayLen)
     *arrayLen += CRC_LEN; // Update the array length to reflect the addition of the CRC
 }
 
-
 /**
  * Computes the checksum (using CRC) for the provided base 8 array.
  *
@@ -99,7 +96,6 @@ int getChecksum(uint8_t *base8Array, int *arrayLen)
     // Calculate and return the CRC as the checksum
     return crc16(base8Array, *arrayLen);
 }
-
 
 /**
  * Uses CRC to verify the integrity of the given array.
@@ -121,9 +117,8 @@ bool verifyDataIntegrity(uint8_t *base8Array, int *arrayLen)
     }
 }
 
-
 /**
- * Manages the reboot sequence, including reading previous status from EEPROM, finding an available log, 
+ * Manages the reboot sequence, including reading previous status from EEPROM, finding an available log,
  * writing reboot causes to logs, and recording a boot message upon sequence completion.
  *
  * @param ptrToStruct    Pointer to the DeviceStatus struct to be updated with reboot sequence details.
@@ -149,9 +144,7 @@ void reboot_sequence(struct DeviceStatus *ptrToStruct, const uint64_t bootTimest
     {
         pushLogToEeprom(ptrToStruct, WATCHDOG_REBOOT, bootTimestamp); // Log the reboot cause
     }
-
     else if (ptrToStruct->rebootStatusCode != 0) // If reboot status code is not 0
-    {
     {
         switch (ptrToStruct->rebootStatusCode)
         {
@@ -187,12 +180,10 @@ void reboot_sequence(struct DeviceStatus *ptrToStruct, const uint64_t bootTimest
             printf("There's gremlins in the code.\n");
             break;
         }
-        
+
+        pushLogToEeprom(ptrToStruct, BOOTFINISHED, bootTimestamp); // TODO: replace magic numbers with enum values.
     }
-
-    pushLogToEeprom(ptrToStruct, BOOTFINISHED, bootTimestamp); // TODO: replace magic numbers with enum values.
 }
-
 
 /**
  * Writes the provided array (assumed to be pre-formatted with appended CRC) to the specified EEPROM address.
@@ -206,13 +197,12 @@ void enterLogToEeprom(uint8_t *base8Array, int *arrayLen, int logAddr)
 {
     // Create a new array and append CRC
     uint8_t crcAppendedArray[*arrayLen + CRC_LEN];
-    memcpy(crcAppendedArray, base8Array, *arrayLen); // Copy the original array
+    memcpy(crcAppendedArray, base8Array, *arrayLen);   // Copy the original array
     appendCrcToBase8Array(crcAppendedArray, arrayLen); // Append CRC to the copied array
 
     // Write the array with appended CRC to the specified EEPROM address
     eeprom_write_page(logAddr, crcAppendedArray, *arrayLen);
 }
-
 
 /**
  * Sets the first byte of every log in EEPROM to 0, indicating that it is not in use.
@@ -231,7 +221,6 @@ void zeroAllLogs()
     }
 }
 
-
 /**
  * Fills a log array based on the given message code and timestamp.
  * The array must have a minimum length of 8.
@@ -243,17 +232,16 @@ void zeroAllLogs()
  */
 int createLogArray(uint8_t *array, int messageCode, uint32_t timestamp)
 {
-    array[0] = 1; // Indicates log in use
+    array[0] = 1;           // Indicates log in use
     array[1] = messageCode; // Store the message code
 
-    array[5] = (uint8_t)(timestamp & 0xFF);               // LSB of timestamp
+    array[5] = (uint8_t)(timestamp & 0xFF); // LSB of timestamp
     array[4] = (uint8_t)((timestamp >> 8) & 0xFF);
     array[3] = (uint8_t)((timestamp >> 16) & 0xFF);
-    array[2] = (uint8_t)((timestamp >> 24) & 0xFF);       // MSB of timestamp
+    array[2] = (uint8_t)((timestamp >> 24) & 0xFF); // MSB of timestamp
 
     return LOG_LEN; // Return the length of the filled log array (LOG_LEN)
 }
-
 
 /**
  * Fills a pill dispenser status log array based on the given pill dispenser state, reboot status code,
@@ -274,21 +262,20 @@ int createPillDispenserStatusLogArray(uint8_t *array, uint8_t pillDispenseState,
     return DISPENSER_STATE_LEN;
 }
 
-
 /**
  * Updates the pill dispenser status in EEPROM based on the provided struct.
  *
  * @param ptrToStruct Pointer to the struct containing the updated pill dispenser status.
  */
-void updatePillDispenserStatus(struct DeviceStatus *ptrToStruct) {
+void updatePillDispenserStatus(struct DeviceStatus *ptrToStruct)
+{
     uint8_t array[LOG_ARR_LEN]; // Buffer to hold the log array
     // Create a log array based on the provided status information
     int arrayLen = createPillDispenserStatusLogArray(array, ptrToStruct->pillDispenseState,
-                                                    ptrToStruct->rebootStatusCode, ptrToStruct->prevCalibStepCount);
+                                                     ptrToStruct->rebootStatusCode, ptrToStruct->prevCalibStepCount);
     // Write the log array to EEPROM at the designated address for pill dispenser status
     enterLogToEeprom(array, &arrayLen, REBOOT_STATUS_ADDR);
 }
-
 
 /**
  * Reads the previous pill dispenser status from EEPROM and updates the provided struct.
@@ -297,8 +284,9 @@ void updatePillDispenserStatus(struct DeviceStatus *ptrToStruct) {
  * @param ptrToStruct Pointer to the struct to update with the pill dispenser status.
  * @return Boolean indicating the success (true) or failure (false) of the EEPROM read operation.
  */
-bool readPillDispenserStatus(struct DeviceStatus *ptrToStruct) {
-    bool eepromReadSuccess = true; // Initialize EEPROM read status as successful
+bool readPillDispenserStatus(struct DeviceStatus *ptrToStruct)
+{
+    bool eepromReadSuccess = true;   // Initialize EEPROM read status as successful
     uint8_t valuesRead[LOG_ARR_LEN]; // Buffer to hold EEPROM values
 
     // Read EEPROM values into the array.
@@ -306,19 +294,21 @@ bool readPillDispenserStatus(struct DeviceStatus *ptrToStruct) {
 
     // Verify data integrity.
     int len = DISPENSER_STATE_ARR_LEN;
-    if (verifyDataIntegrity(valuesRead, &len) == true) {
+    if (verifyDataIntegrity(valuesRead, &len) == true)
+    {
         // Data integrity verified, extract and assign values to struct fields.
         ptrToStruct->pillDispenseState = valuesRead[PILL_DISPENSE_STATE];
         ptrToStruct->rebootStatusCode = valuesRead[REBOOT_STATUS_CODE];
         ptrToStruct->prevCalibStepCount = (uint16_t)valuesRead[PREV_CALIB_STEP_COUNT_MSB] << 8; // Extract MSB
         ptrToStruct->prevCalibStepCount |= (uint16_t)valuesRead[PREV_CALIB_STEP_COUNT_LSB];     // Extract LSB
-    } else {
+    }
+    else
+    {
         eepromReadSuccess = false; // Data integrity verification failed
     }
 
     return eepromReadSuccess; // Return the success/failure status of the EEPROM read operation
 }
-
 
 /**
  * Finds the first available log entry in EEPROM.
@@ -327,13 +317,16 @@ bool readPillDispenserStatus(struct DeviceStatus *ptrToStruct) {
  *
  * @return The index of the first available log entry if found, else returns 0 after clearing all logs.
  */
-int findFirstAvailableLog() {
-    int count = 0; // Initialize counter
+int findFirstAvailableLog()
+{
+    int count = 0;        // Initialize counter
     uint16_t logAddr = 0; // Initialize EEPROM address
 
     // Find the first available log entry
-    while (count <= MAX_LOGS) {
-        if (eeprom_read_byte(logAddr) == 0) {
+    while (count <= MAX_LOGS)
+    {
+        if (eeprom_read_byte(logAddr) == 0)
+        {
             return count; // Return index if an available log entry is found
         }
         logAddr += LOG_SIZE; // Move to the next log entry
@@ -341,10 +334,8 @@ int findFirstAvailableLog() {
     }
 
     zeroAllLogs(); // Clear all logs if all entries are full
-    return 0; // Return 0 after clearing logs
+    return 0;      // Return 0 after clearing logs
 }
-
-
 
 /**
  * Calculates the elapsed time in seconds since the system boot using a boot timestamp.
@@ -352,12 +343,11 @@ int findFirstAvailableLog() {
  * @param bootTimestamp The timestamp representing the system boot time.
  * @return The elapsed time in seconds since the system boot based on the provided boot timestamp.
  */
-uint32_t getTimestampSinceBoot(const uint64_t bootTimestamp) {
+uint32_t getTimestampSinceBoot(const uint64_t bootTimestamp)
+{
     // Calculate the elapsed time in microseconds since boot, then convert it to seconds
     return (uint32_t)((time_us_64() - bootTimestamp) / 1000000);
 }
-
-
 
 /**
  * Writes a log entry to EEPROM using information from the device status structure,
@@ -366,47 +356,51 @@ uint32_t getTimestampSinceBoot(const uint64_t bootTimestamp) {
  * @param messageCode               Message code indicating the type of log entry.
  * @param bootTimestamp             Boot timestamp representing the time when the log was created.
  */
-void pushLogToEeprom(struct DeviceStatus *pillDispenserStatusStruct, int messageCode, uint64_t bootTimestamp) {
+void pushLogToEeprom(struct DeviceStatus *pillDispenserStatusStruct, int messageCode, uint64_t bootTimestamp)
+{
     uint8_t logArray[LOG_LEN]; // Buffer to hold log data
     // Create a log array with the provided message code and boot timestamp
     int arrayLen = createLogArray(logArray, messageCode, getTimestampSinceBoot(bootTimestamp));
 
     // Write the log array to EEPROM at the appropriate index based on the log size and unused log index
     enterLogToEeprom(logArray, &arrayLen, (pillDispenserStatusStruct->unusedLogIndex * LOG_SIZE));
-    
+
     // Update the unused log index in the device status structure
     updateUnusedLogIndex(pillDispenserStatusStruct);
 }
-
-
 
 /**
  * Updates the unused log index in the device status structure.
  *
  * @param pillDispenserStatusStruct Pointer to the device status structure containing log-related information.
  */
-void updateUnusedLogIndex(struct DeviceStatus *pillDispenserStatusStruct) {
-    if (pillDispenserStatusStruct->unusedLogIndex < MAX_LOGS) {
+void updateUnusedLogIndex(struct DeviceStatus *pillDispenserStatusStruct)
+{
+    if (pillDispenserStatusStruct->unusedLogIndex < MAX_LOGS)
+    {
         pillDispenserStatusStruct->unusedLogIndex++;
-    } else {
-        zeroAllLogs(); // Reset all logs when the unused log index reaches the maximum limit
+    }
+    else
+    {
+        zeroAllLogs();                                 // Reset all logs when the unused log index reaches the maximum limit
         pillDispenserStatusStruct->unusedLogIndex = 0; // Reset unused log index to 0
     }
 }
 
-
-
 /**
  * Prints valid logs stored in EEPROM by reading and interpreting log data.
  */
-void printValidLogs() {
-    for (int i = 0; i < MAX_LOGS; i++) {
+void printValidLogs()
+{
+    for (int i = 0; i < MAX_LOGS; i++)
+    {
         uint16_t logAddr = i * LOG_SIZE; // Calculate the EEPROM address for the log entry
-        uint8_t logData[LOG_LEN]; // Buffer to hold log data
+        uint8_t logData[LOG_LEN];        // Buffer to hold log data
 
         eeprom_read_page(logAddr, logData, LOG_LEN); // Read log data from EEPROM
 
-        if (logData[0] != 0) { // Check if the log entry is valid (non-zero message code)
+        if (logData[0] != 0)
+        {                                     // Check if the log entry is valid (non-zero message code)
             uint8_t messageCode = logData[1]; // Extract the message code
             uint32_t timestamp = (logData[2] << 24) | (logData[3] << 16) | (logData[4] << 8) | logData[5];
             // Construct timestamp from individual bytes
@@ -417,7 +411,6 @@ void printValidLogs() {
     }
 }
 
-
 /**
  * Checks if a given value exists within an array.
  *
@@ -426,9 +419,12 @@ void printValidLogs() {
  * @param size The size of the array.
  * @return true if the value is found in the array, otherwise false.
  */
-bool isValueInArray(int value, int *array, int size) {
-    for (int i = 0; i < size; i++) {
-        if (array[i] == value) {
+bool isValueInArray(int value, int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+        {
             return true; // Value found in the array
         }
     }
